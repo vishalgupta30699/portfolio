6811d91162cb4439d1a9afb2bf9ead11b6050509
vishalgupta30699 <vishalg30699@gmail.com>
2020-09-27 05:26:50 +0530

    Updates

---
+++ precache-manifest.4d48a15d00037d9ea6fcf2dba62148d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f84d8d8d951e732e80f219cd3e129e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d8f0f2cd437d80df549",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/portfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8f0f2cd437d80df549",
+    "url": "/portfolio/static/js/main.1ca63180.chunk.js"
+  },
+  {
+    "revision": "d6ca8614ab4df5b1bf68",
+    "url": "/portfolio/static/js/runtime-main.44645aae.js"
+  }
+]);
